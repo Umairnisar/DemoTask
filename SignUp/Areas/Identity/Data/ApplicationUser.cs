e495bb4880846f8025040038e5b0b8de017ecebe
Umair Nisar <umairnisar74@gmail.com>
2022-01-09 17:56:23 +0500

    Register and Login Functionality

---
+++ SignUp/Areas/Identity/Data/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SignUp.Areas.Identity.Data
+{
+    public class ApplicationUser : IdentityUser
+    
+    {
+     
+            [PersonalData]
+            [Column(TypeName = "nvarchar(100)")]
+            public string FirstName { get; set; }
+
+            [PersonalData]
+            [Column(TypeName = "nvarchar(100)")]
+            public string LirstName { get; set; }
+
+        
+    }
+}
